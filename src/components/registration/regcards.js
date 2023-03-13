136085d4a32791cbd483fb8798ab07cb2b20e384
zahaanshapoorjee <zahaan@shapoorjee.com>
2023-03-14 05:25:33 +0530

    TEST

--- src/components/registration/regcards.js
+++ src/components/registration/regcards.js
@@ -47,6 +47,7 @@ export default function RegCards(){
                                 src={JoseMourinho}
                                 width="100%"
                                 height="100%"
+                                objectFit="cover"
                                 alt="Card example background"
                             />
                         </Card.Body>
