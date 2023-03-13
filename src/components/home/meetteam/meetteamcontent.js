5063bee005ee2351b82dea1a16a61e56d6a53c91
zahaanshapoorjee <zahaan@shapoorjee.com>
2023-03-14 02:17:03 +0530

    test

--- src/components/home/meetteam/meetteamcontent.js
+++ src/components/home/meetteam/meetteamcontent.js
@@ -181,7 +181,8 @@ export default function MeetTeamContent () {
                 <Grid.Container gap={1}
                 css={{
                     jc: 'center',
-                    textAlign: 'center'
+                    textAlign: 'center',
+                    width:'50%'
                 }}>
                     <Grid>
                         <Image src={P4} className='team-pic' />
