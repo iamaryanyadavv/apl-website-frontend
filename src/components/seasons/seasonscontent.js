b35fac8db2544d4474598bb9c130e83a1a347cbe
Aryan Yadav <iamaryanyadav@Yaddoos-Macbook.local>
2023-03-24 15:48:28 +0530

    seasons card apl5 background image fix

--- src/components/seasons/seasonscontent.js
+++ src/components/seasons/seasonscontent.js
@@ -2,7 +2,7 @@ import { Grid } from "@nextui-org/react";
 import React from "react";
 import SeasonCard from "./seasoncard";
 import APL4Cover from '../../assets/images/APL4/APL4Cover.JPG';
-import APL5Cover from '../../assets/images/APL5/APL5Cover.jpg';
+import APL5Cover from '../../assets/images/APL5/APL5Cover 2.jpg';
 
 export default function SeasonsContent(){
     return(
