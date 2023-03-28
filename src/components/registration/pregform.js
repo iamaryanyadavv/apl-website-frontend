6839d7fdd172b46f3e7e99880080739aa1cef63f
zahaanshapoorjee <zahaan@shapoorjee.com>
2023-03-28 13:58:26 +0530

    Player Registration Male limit increased to 192

--- src/components/registration/pregform.js
+++ src/components/registration/pregform.js
@@ -371,7 +371,7 @@ export default function PRegForm(){
                     
                 };
                 console.log(count);
-                if(count>=160)
+                if(count>=192)
                 {
                     setmaleRegFull(true);
                     alert('Male Player Registrations are full!')
