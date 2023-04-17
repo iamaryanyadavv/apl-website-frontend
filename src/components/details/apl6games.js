97c3c2a2ddb8328782824a6b04eee794622a0bda
Aryan Yadav <iamaryanyadav@Yaddoos-Macbook.local>
2023-04-17 18:41:17 +0530

    fixed 12;00 FXITURES

--- src/components/details/apl6games.js
+++ src/components/details/apl6games.js
@@ -6,6 +6,7 @@ export default function Games(){
     const [Teams, setTeams] = useState();
     const [Day1Games, setDay1Games] = useState([]);
     const [Day2Games, setDay2Games] = useState([]);
+    const [Day2KOGames, setDay2KOGames] = useState([]);
     const [Day3Games, setDay3Games] = useState([]);
     const [Day1, setDay1] = useState(true);
     const [Day2, setDay2] = useState(false);
@@ -17,14 +18,19 @@ export default function Games(){
         .then(data=>{
             const day1games=[]
             const day2games=[]
+            const day2kogames=[]
             const day3games=[]
-            for(var i=1; i<24;i++)
+            for(var i=1; i<25;i++)
             {
                 day1games.push(data.values[i])
             }
-            for(var i=26; i<45;i++)
+            for(var i=26; i<38;i++)
             {
                 day2games.push(data.values[i])
+            }
+            for(var i=38; i<46;i++)
+            {
+                day2kogames.push(data.values[i])
             }
             for(var i=47; i<=54;i++)
             {
@@ -32,6 +38,7 @@ export default function Games(){
             }
             setDay1Games(day1games)
             setDay2Games(day2games)
+            setDay2KOGames(day2kogames)
             setDay3Games(day3games.reverse())
             setFetching(false)
         })
@@ -41,7 +48,6 @@ export default function Games(){
         await fetch('https://aplapi.onrender.com/seasons/apl6/teamdata')
         .then(response=>response.json())
         .then(data=>{
-            console.log(data)
             setTeams(data)
         })
     }
@@ -273,7 +279,7 @@ export default function Games(){
                         </Grid.Container>
                     }
 
-                    {Day2Games.length!=0 && Day2 &&
+                    {Day2Games.length!=0 && Day2 && Day2KOGames.length!=0 &&
                         <Grid.Container 
                         css={{
                             jc: 'center',
@@ -395,7 +401,137 @@ export default function Games(){
                                                     <Card.Divider />
                                                     
                                                     <Card.Footer>
-                                                        <Text b>April 22nd Saturday {game[0]}</Text>
+                                                        {game[0]!='12:00 AM' && game[0]!='12:30 AM' &&
+                                                            <Text b>April 22nd Saturday {game[0]}</Text>
+                                                        }
+                                                        {game[0]=='12:00 AM' &&
+                                                            <Text b>April 23rd Sunday {game[0]}</Text>
+                                                        }
+                                                        {game[0]=='12:30 AM' &&
+                                                            <Text b>April 23rd Sunday {game[0]}</Text>
+                                                        }
+                                                    </Card.Footer>
+                                                </Card>
+                                            </Grid>
+                                        ))}
+                                        {Day2KOGames.map((game,index)=>(
+                                            <Grid key={index}
+                                            css={{
+                                                padding: '10px'
+                                            }}>
+                                                <Card css={{ mw: "max-content" }}>
+                                                    <Card.Header>
+                                                        <Text b>{game[4]}</Text>
+                                                    </Card.Header>
+
+                                                    <Card.Divider />
+
+                                                    <Card.Body css={{ py: "$10", padding: '20px 10px' }}>
+                                                        <Grid.Container
+                                                        css={{
+                                                            jc: 'center',
+                                                            textAlign: 'center'
+                                                        }}>
+                                                            <Grid
+                                                            css={{
+                                                                '@xsMax':{
+                                                                    width: '100%'
+                                                                },
+                                                                '@xsMin':{
+                                                                    width: '300px',
+                                                                }
+                                                            }}>
+                                                                <Row 
+                                                                css={{
+                                                                    jc: 'center',
+                                                                    textAlign: 'center',
+                                                                    alignItems: 'center'
+                                                                }}>
+                                                                    {Teams.map((team,index)=>(
+                                                                        team[2]==game[1]? <Avatar src={team[0]}/> : <></>
+                                                                    ))}
+                                                                    <Text
+                                                                    css={{
+                                                                        fontSize: '$2xl',
+                                                                        fontWeight: '$semibold',
+                                                                        padding: '0px 10px'
+                                                                    }}>
+                                                                        {game[1]}
+                                                                    </Text>
+                                                                </Row>
+                                                            </Grid>
+
+                                                            <Grid 
+                                                            css={{
+                                                                '@xsMax':{
+                                                                    width: '100%'
+                                                                },
+                                                                '@xsMin':{
+                                                                    width: '100px',
+                                                                    alignItems: 'center'
+                                                                }
+                                                            }}>
+                                                                <Text 
+                                                                css={{
+                                                                    fontSize: '$2xl',
+                                                                    fontWeight: '$semibold',
+                                                                    padding: '0px 10px',
+                                                                    display: 'inline'
+                                                                }}>
+                                                                    {game[3]}
+                                                                </Text>
+                                                            </Grid>
+
+                                                            <Grid 
+                                                            css={{
+                                                                '@xsMax':{
+                                                                    width: '100%'
+                                                                },
+                                                                '@xsMin':{
+                                                                    width: '300px',
+                                                                }
+                                                            }}>
+                                                                <Row 
+                                                                css={{
+                                                                    jc: 'center',
+                                                                    textAlign: 'center',
+                                                                    alignItems: 'center'
+                                                                }}>
+                                                                    <Text 
+                                                                    css={{
+                                                                        fontSize: '$2xl',
+                                                                        fontWeight: '$semibold',
+                                                                        padding: '0px 10px'
+                                                                    }}>
+                                                                        {game[2]}
+                                                                    </Text>
+                                                                    {Teams.map((team,index)=>(
+                                                                        team[2]==game[2]? <Avatar src={team[0]}/> : <></>
+                                                                    ))}
+                                                                </Row>
+                                                            </Grid>
+                                                        </Grid.Container>
+                                                        <Grid.Container
+                                                        css={{
+                                                            jc: 'center',
+                                                            textAlign: 'center',
+                                                            alignItems: 'center'
+                                                        }}>
+                                                            <Text
+                                                            css={{
+                                                                color:'$gray600'
+                                                            }}>
+                                                                {game[5]}
+                                                            </Text>
+                                                        </Grid.Container>
+
+
+                                                    </Card.Body>
+                                                    
+                                                    <Card.Divider />
+                                                    
+                                                    <Card.Footer>
+                                                        <Text b>April 23rd Sunday {game[0]}</Text>
                                                     </Card.Footer>
                                                 </Card>
                                             </Grid>
@@ -527,7 +663,12 @@ export default function Games(){
                                                     <Card.Divider />
                                                     
                                                     <Card.Footer>
-                                                        <Text b>April 21st Friday {game[0]}</Text>
+                                                        {game[0]!='12:00 AM' && 
+                                                            <Text b>April 21st Friday {game[0]}</Text>
+                                                        }
+                                                        {game[0]=='12:00 AM' &&
+                                                            <Text b>April 22nd Saturday {game[0]}</Text>
+                                                        }
                                                     </Card.Footer>
                                                 </Card>
                                             </Grid>
