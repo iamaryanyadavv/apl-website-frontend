e8c1830c1cbf767991ec2efde445cb2e6e64d706
zahaanshapoorjee <zahaan@shapoorjee.com>
2024-03-25 16:51:29 +0530

    Registration Prices updated

--- src/components/registration/pregform.js
+++ src/components/registration/pregform.js
@@ -1446,7 +1446,7 @@ export default function PRegForm(){
                                     fontSize: '$xl',
                                     fontWeight: '$medium'
                                 }}>
-                                    Please pay the amount (&#x20B9;200) to Uday Srivastava, via PayTM or GPay.
+                                    Please pay the amount (&#x20B9;250) to Uday Srivastava, via PayTM or GPay.
                                 </Text>
                                 <Text 
                                 css={{
